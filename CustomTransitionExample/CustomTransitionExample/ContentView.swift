0223bd0cb60c2449e90986b0c5445000af9a3f73
Lucas Pereira <lucasgpereira.10@gmail.com>
2022-04-01 14:48:29 +0200

    Initializing project

---
+++ CustomTransitionExample/CustomTransitionExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomTransitionExample
+//
+//  Created by Lucas Pereira on 01/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomTransitionExample/CustomTransitionExample/CustomTransitionExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTransitionExampleApp.swift
+//  CustomTransitionExample
+//
+//  Created by Lucas Pereira on 01/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomTransitionExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
